c8938ca32934b980ffdfe1d57483b274792f7743
mhtsort <mhtsort@gmail.com>
2023-04-22 03:42:50 +0300

    Greek text added

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from datetime import datetime
 today: date = date.today()
 time: datetime = datetime.now()
 print(f'{today} {time.strftime("%H:%M")}  :: Successfully run main')
+print("Greek characters and Ελληνικοί Χαρακτήρες μφχθζλξ")
